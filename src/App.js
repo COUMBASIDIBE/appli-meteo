8b7bb70a2ad3706a40019f25d45bd229b38b5b67
COUMBASIDIBE <coumba.sidibe@hotmail.fr>
2021-12-28 16:15:54 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
+
+  const apiKey =''
+  const [weatherData, setWeatherData]= useState([{}])
+  const [city, setCity]= useState("") 
+
+  const getWeather =(event) => { 
+    if(event.key =="Enter") {
+      fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city},fr&units=metric&lang=fr&appid=${apiKey}&lang={fr}`)
+      .then(response =>response.json())
+      .then(
+        data=>{
+          setWeatherData(data)
+        
+          setCity("")
+        
+      
+        }
+      )
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input 
+      className='input' 
+      placeholder="Entrer le nom d'une ville"
+      onChange={e => setCity(e.target.value)}
+      value={city}
+      onKeyPress={getWeather}
+      />
+      {typeof weatherData.main === 'undefined'? (
+      <div>
+        <p>Bienvenu sur votre site  méteo</p>
+      </div>
+    ): (
+      <div className='weather-card'>
+        <p className='city'>Ville : {weatherData.name}</p>
+        <p className='temp'>Température : {Math.round(weatherData.main.temp)} degrés</p>
+        <p className='des'>Description : {weatherData.weather[0].description}</p>
+      </div> 
+    )}
+
+      {weatherData.cod==="404"?(
+        <p>ville non trouvée, merci de saisir une autre ville</p>
+      ):
+      (
+        <></>
+      )}
+
     </div>
   );
-}
+};
 
 export default App;
